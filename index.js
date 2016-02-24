1db2e6dd12f8620a780ceacdb36600a2638b3b8e
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-24 22:01:36 +0100

    Startet to move conig out

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var merge = require('lodash.merge');
-var defaultConfig = require('./config');
 
 module.exports = {
-	tasks: require('./tasks'),
-	extendTasks: require('./utils/extend-tasks'),
-	extendConfig: require('./utils/extend-config')
+    tasks: require('./tasks'),
+    extendTasks: require('./utils/extend-tasks'),
 };
