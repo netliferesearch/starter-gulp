1cd08845c0ef085aea85b870b8844a25e1e313ae
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-25 20:50:59 +0100

    removed requiore on watch tasks

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,8 +5,8 @@ var gulp = require('gulp');
 var config = require('../config');
 
 module.exports = function() {
-    gulp.watch(config.src.scss, [sass]);
-    gulp.watch(config.src.js, [browserify]);
-    gulp.watch(config.src.images, [images]);
-    gulp.watch(config.src.icons, [inline-svg]);
+    gulp.watch(config.src.scss, ['sass']);
+    gulp.watch(config.src.js, ['browserify']);
+    gulp.watch(config.src.images, ['images']);
+    gulp.watch(config.src.icons, ['inline-svg']);
 };
