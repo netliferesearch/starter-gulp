4edd30a08822e3a9c2571aa87f28ef39db7b22f6
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-26 19:01:49 +0100

    corrected path

--- tasks/watch.js
+++ tasks/watch.js
@@ -10,5 +10,5 @@ module.exports = function() {
     gulp.watch(config.src.js, ['browserify']);
     gulp.watch(config.src.images, ['images']);
     gulp.watch(config.src.icons, ['inline-svg']);
-    gulp.watch(config.src.html).on('change', browserSync.reload);
+    gulp.watch(config.dist.html).on('change', browserSync.reload);
 };
