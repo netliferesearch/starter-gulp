5c4e2999b930482693d749d5a0ca62489987b22f
Fredrik Jensen <fredrik@netliferesearch.com>
2016-05-03 13:23:37 +0200

    Removed imagemin

--- tasks/images.js
+++ tasks/images.js
@@ -1,14 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin');
+    newer = require('gulp-newer');
 
 var config = require('../config');
 
 module.exports = function() {
     return gulp.src(config.src.images)
         .pipe(newer(config.dist.images))
-        .pipe(imagemin())
         .pipe(gulp.dest(config.dist.images));
 };
