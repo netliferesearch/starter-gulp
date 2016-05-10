c9b7c1e669507ebd8887433d8268b24b21c3a5f4
Fredrik Jensen <fredrik@netliferesearch.com>
2016-05-10 22:23:26 +0200

    restoring

--- tasks/index.js
+++ tasks/index.js
@@ -1,26 +1,26 @@
 'use strict';
 
 module.exports = {
-	// Browser sync
-	'browser-sync': [require('./browser-sync')],
+    // Browser sync
+    'browser-sync': [require('./browser-sync')],
 
-	// Assets
-	'images': [require('./images')],
-	'inline-svg': [require('./inline-svg')],
-	'assets': [['images', 'inline-svg']],
+    // Assets
+    'images': [require('./images')],
+    'inline-svg': [require('./inline-svg')],
+    'assets': [['images', 'inline-svg']],
 
-	// Linting & statistic
-	'lint-sass': [require('./lint-sass')],
-	'lint-js': [require('./lint-js')],
-	'css-stats': [require('./css-stats')],
+    // Linting & statistic
+    'lint-sass': [require('./lint-sass')],
+    'lint-js': [require('./lint-js')],
+    'css-stats': [require('./css-stats')],
 
-	// Build
-	'sass': [require('./sass')],
-    'javascript': [require('./javascript')],
-	'gh-pages': [require('./gh-pages')],
-	'watch': [require('./watch')],
+    // Build
+    'sass': [require('./sass')],
+    'javascript': [['lint-js'], require('./javascript')],
+    'gh-pages': [require('./gh-pages')],
+    'watch': [require('./watch')],
 
-	'build': [['assets', 'javascript', 'sass']],
-	'default': [['build', 'browser-sync', 'watch']],
-	'deploy': [['assets', 'build', 'sass', 'gh-pages']]
+    'build': [['assets', 'javascript', 'sass']],
+    'default': [['build', 'browser-sync', 'watch']],
+    'deploy': [['assets', 'build', 'sass', 'gh-pages']]
 };
